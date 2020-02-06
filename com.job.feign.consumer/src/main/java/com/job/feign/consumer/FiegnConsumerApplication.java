a69cb07f6fc3798d8fc9adc3f567838d6998c801
lijiawen <lijiawen@jiuqi.com.cn>
2020-02-06 15:36:03 +0800

    【测试换行符】

--- com.job.feign.consumer/src/main/java/com/job/feign/consumer/FiegnConsumerApplication.java
+++ com.job.feign.consumer/src/main/java/com/job/feign/consumer/FiegnConsumerApplication.java
@@ -3,7 +3,12 @@ package com.job.feign.consumer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-
+/**
+ * 
+ * <p>TODO 启动类</p>
+ * @author lijiawen
+ * @version 2020年2月6日
+ */
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.job.feign.consumer.*"})
 public class FiegnConsumerApplication {
